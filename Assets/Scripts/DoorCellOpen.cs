bdd313336d235ebeeb8e5e10713f966749ddea52
Barnett Chengberlin <b.chengberlin2626@gmail.com>
2022-04-28 10:17:44 -0400

    first commit

---
+++ Assets/Scripts/DoorCellOpen.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorCellOpen : MonoBehaviour
+{
+    public float TheDistance;
+    public GameObject ActionDisplay;
+    public GameObject ActionText;
+    public GameObject TheDoor;
+    public AudioSource CreakSound;
+
+    void Update()
+    {
+        TheDistance = PlayerCasting.DistanceFromTarget;
+    }
+
+    void OnMouseOver() {
+        if (TheDistance <= 3) {
+            ActionDisplay.SetActive(true);
+            ActionText.SetActive(true);
+        }
+        if (Input.GetButtonDown("Action") && TheDistance <= 3) {
+            this.GetComponent<BoxCollider>().enabled = false;
+            ActionDisplay.SetActive(false);
+            ActionText.SetActive(false);
+            TheDoor.GetComponent<Animation>().Play("FirstDoorOpenAnim");
+            CreakSound.Play();
+        }
+    }
+
+    void OnMouseExit() {
+        ActionDisplay.SetActive(false);
+        ActionText.SetActive(false);
+    }
+}
